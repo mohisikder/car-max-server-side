53d6c3bfe28b9e8bf4b934174c32a95597492cc4
Mohiuddin Sikder <mohi.info20@gmail.com>
2021-11-13 18:12:31 +0600

    database added

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zz9qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+
 
 async function run() {
   try {
     await client.connect();
-    const database = client.db("dream_travel_db");
+    const database = client.db('car_max_db');
     const productsCollection = database.collection("products");
     const orderCollection = database.collection("user_order");
 
@@ -27,57 +28,18 @@ async function run() {
        res.send("Hello carMax")
     })
     
-    // POST API (Add Product)
-    app.post('/addproduct', async(req, res)=>{
-      const newProduct = (req.body);
+    // GET API (get Product)
+    app.get('/products', async(req, res)=>{
+       const result= await productsCollection.find({}).toArray()
+       res.send(result)
+    })
+
+   //  Add Product(POST API)
+   app.post('/addproduct', async(req, res)=>{
+      const newProduct = req.body
       const result = await productsCollection.insertOne(newProduct)
       res.json(result)
-    })
-
-    // ADD ORDER POST
-    app.post('/addorder', async(req, res)=>{
-      const newOrder = (req.body)
-      const result = await orderCollection.insertOne(newOrder)
-      res.json(result)
-      console.log(result);
-    })
-  
-
-    // GET MY ORDER
-    app.get('/myorder/:email', async(req, res)=>{
-      const query = (req.params.email)
-      const result = await orderCollection.find({email : (query)}).toArray()
-      res.send(result);
-    })
-
-    // GET API
-    app.get('/products', async(req,res)=>{
-      const result = await productsCollection.find({}).toArray()
-      res.send(result)
-    })
-
-    // GET ALL Order API
-    app.get('/allorder', async(req,res)=>{
-      const result = await productsCollection.find({}).toArray()
-      res.send(result)
-    })
-
-    // Delete ALL Booking API
-    app.delete('/allorder/:id', async (req,res) => {
-      const id = req.params.id
-      const query = {_id: ObjectId(id)}
-      const result = await orderCollection.deleteOne(query)
-      res.send(result)
-    })
-
-    // DELETE BOOKING
-
-    app.delete('/order/:id', async (req, res) => {
-      const id = req.params.id
-      const query = {_id: ObjectId(id)}
-      const result = await orderCollection.deleteOne(query)
-      res.json(result)
-    });
+   })
    
   } finally {
     // await client.close();
